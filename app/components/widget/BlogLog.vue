0ab20e95dcd571814455833a2dfd186927be7c34
YWYdog <pz9666@qq.com>
2026-05-19 20:55:07 +0800

    更新更新日志

--- app/components/widget/BlogLog.vue
+++ app/components/widget/BlogLog.vue
@@ -2,8 +2,9 @@
 const { timeEstablished } = useAppConfig()
 
 const blogLog = [
-	{ label: '2026-05-05', value: '开始搭建网站' },
-	{ label: timeEstablished, value: '发布第一篇文章' },
+    { label: '2026-05-20', value: '博客搭建完成' },
+    { label: '2026-05-19', value: '发布第一篇文章' },
+	{ label: '2026-05-05', value: '开始搭建博客' },
 ]
 </script>
 
